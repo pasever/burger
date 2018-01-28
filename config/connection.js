557133a4e14b10a3205d2c14b9a5e0a8b245184c
Pasever <pas1034ever@gmail.com>
2018-01-27 23:49:59 -0500

    a.2.3

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require("mysql");
-// const connection = mysql.createConnection({
-//     // port: PROCESS.ENV.PORT || 3306,
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-//   });
 
-const connection = mysql.createConnection('mysql://kthc846nvk1a0zam:vavq9smupzswm2sh@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kw2xnpn2lq6yo56u')
+let connection;
+
+if(process.env.JAWSDB_URL) {
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+      connection = mysql.createConnection({
+          host: "localhost",
+          user: "root",
+          password: "",
+          database: "burgers_db",
+      });
+  };
+
+connection = mysql.createConnection('mysql://kthc846nvk1a0zam:vavq9smupzswm2sh@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kw2xnpn2lq6yo56u')
 
 connection.connect(err => {
   if (err) {
